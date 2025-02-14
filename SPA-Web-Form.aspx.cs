b6e8e8480d132cd4357b8f1915405f65d7b5293b
ietzel <67884277+ietzel@users.noreply.github.com>
2025-02-14 11:39:30 -0500

    Update SPA-Web-Form.aspx.cs

--- SPA-Web-Form.aspx.cs
+++ SPA-Web-Form.aspx.cs
@@ -5,12 +5,9 @@ using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 
-namespace SoftwareProcessAid
-{
-    public partial class SPA_Web_Form : System.Web.UI.Page
-    {
-        protected void Page_Load(object sender, EventArgs e)
-        {
+namespace SoftwareProcessAid {
+    public partial class SPA_Web_Form : System.Web.UI.Page {
+        protected void Page_Load(object sender, EventArgs e) {
 
         }
     }
